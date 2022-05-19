f52e0a217bc30452950b87c6d7c5bf5f97840763
Afridi Azmain <Azmain951>
2022-05-19 19:28:18 +0600

    setting up firebase file and environment variable added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -7,13 +7,13 @@ const AvailableAppointments = ({ date }) => {
     const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
             .then(res => res.json())
             .then(data => setServices(data));
     }, []);
 
     return (
-        <div className='mt-12'>
+        <div className='my-12'>
             <h4 className='text-secondary text-center text-xl'>Available Appointments on: {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10'>
                 {

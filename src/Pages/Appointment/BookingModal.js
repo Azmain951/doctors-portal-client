a2ddd506333e5255d1ba5f1ee5857e302a0fa947
Afridi Azmain <Azmain951>
2022-05-20 18:58:23 +0600

    used react query to update data automatically

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,16 +1,48 @@
 import { format } from 'date-fns';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 
-const BookingModal = ({ treatment, date, setTreatment }) => {
-    const { name, slots } = treatment;
+const BookingModal = ({ treatment, date, setTreatment, refetch }) => {
+    const { _id, name, slots } = treatment;
     const [user] = useAuthState(auth);
+    const formattedDate = format(date, "PP");
     const handleBooking = e => {
         e.preventDefault();
         const slot = e.target.slot.value;
-        console.log(name, slot)
-        setTreatment(null);
+        console.log(name, slot);
+        const booking = {
+            treatmentId: _id,
+            treatment: name,
+            date: formattedDate,
+            slot,
+            patient: user.email,
+            patientName: user.displayName,
+            phone: e.target.phone.value
+        }
+
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.success) {
+                    toast.success(`Appointment is set ${formattedDate} at ${slot}`);
+
+                }
+                else {
+                    toast.error(`Already have an Appointment on, ${data.booking?.date} at ${data.booking?.slot}`);
+                }
+                refetch();
+                setTreatment(null);
+
+            })
     }
 
     return (
